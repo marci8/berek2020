57751fa2a576a638663013de7ed2ba6c719d07c4
gaspar.marcell <gaspar.marcell@28-DIAK13>
2024-11-18 14:56:14 +0100

    Add project files.

---
+++ Ber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020;
+
+internal class Ber
+{
+    private string nev;
+    private string neme;
+    private string reszleg;
+    private int belepes;
+    private int bere;
+
+    public string Nev { get => nev; set => nev = value; }
+    public string Neme { get => neme; set => neme = value; }
+    public string Reszleg { get => reszleg; set => reszleg = value; }
+    public int Belepes { get => belepes; set => belepes = value; }
+    public int Bere { get => bere; set => bere = value; }
+
+    public Ber(string sor)
+    {
+        string[] s = sor.Split(";");
+        nev = s[0];
+        neme = s[1];
+        reszleg = s[2];
+        belepes = int.Parse(s[3]);
+        bere = int.Parse(s[4]);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using berek2020;
+using System.Text;
+
+List<Ber> berek = [];
+using StreamReader sr = new("..\\..\\..\\src\\berek2020.txt", Encoding.UTF8);
+sr.ReadLine();
+while (!sr.EndOfStream) berek.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. Feladat: Dolgozók száma: {berek.Count} fő");
+
+var f4 = berek.Average(b => b.Bere);
+Console.WriteLine($"4. Feladat: Bérek átlaga: {f4/1000:0.00} eFt");
+
+Console.Write("5. Feladat: Kérem a részleg nevét: ");
+string resz = Console.ReadLine();
+
+int max = 0;
+int db = 0;
+for (int i = 0; i<berek.Count; i++)
+{
+    if (resz.ToLower() == berek[i].Reszleg)
+    {
+        if (berek[i].Bere > berek[max].Bere) { max = i; }
+    }
+    else { db++; }
+}
+if (db == berek.Count)
+{
+    Console.WriteLine("A megadott részleg nem létezik a cégnél!");
+}
+else
+{
+    Console.WriteLine($"6. Feladat: A legtöbbet kereső dolgozó a megadott részlegen\n" +
+        $"\tNév: {berek[max].Nev}\n" +
+        $"\tNem: {berek[max].Neme}\n" +
+        $"\tBelépés: {berek[max].Belepes}\n" +
+        $"\tBér: {berek[max].Bere} forint");
+}
+
+var f7 = berek.GroupBy(b => b.Reszleg);
+foreach (var f in f7)
+{
+    Console.WriteLine($"\t{f.Key} - {f.Count()} fő");
+}
